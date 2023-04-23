b6a1315a717e2abfcfe731637efec206b22b5620
WanderingSnail <seongjin123.SY@gmail.com>
2023-04-23 01:07:49 -0700

    IDK what i modified

--- frontend/components/RideInfo-Driver/RideInfo-Driver.js
+++ frontend/components/RideInfo-Driver/RideInfo-Driver.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
-import NavOptions from '../NavOptions/NavOptions';
+import Map from '../Map/Map';
+import Card from '../Card/Card';
+import MapView, { Marker } from 'react-native-maps';
+import tw from 'twrnc';
 
-const RideInfoD = ({ route }) => {
-    // const { lat1 } = route.lat1;
-    // const { lon1 } = route.lon1;
-    // const { lat2 } = route.lat2;
-    // const { lon2 } = route.lon2;
-
+const RideInfoC = ({ route }) => {
     return (
         <SafeAreaView>
-            <View style={{ padding: 5 }}>
-                <Text>This is the RideInfo-Driver page</Text>
-                {/* <Text>{lat1}</Text>
-                <Text>{lon1}</Text>
-                <Text>{lat2}</Text>
-                <Text>{lon2}</Text> */}
+            <View style>
+                {/* <Text>This is the RideInfo-Customer page</Text> */}
+                <View style={tw`h-1/2`}>
+                    <Map />
+                </View >
+                <View style={tw`h-1/2`}>
+                    <Card />
+                </View>
+
+                {/* <View style={styles.container}> </View> */}
             </View>
-        </SafeAreaView>
+        </SafeAreaView >
     )
 }
 
-export default RideInfoD;
+export default RideInfoC;
 
 const styles = StyleSheet.create({
-    // text: {
-    //     color: 'blue',
-    // },
+    // container: {
+    //     position: 'absolute',
+    //     top: '50%',
+    //     width: '100 %',
+    // transform: 'translateY(-50 %)'
+    // }
 })
